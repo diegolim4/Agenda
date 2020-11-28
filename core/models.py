7eb084bebf0c75132fdfa58525dfcc56f2bf5c7f
Diego Lima <lima.j.diego@gmail.com>
2020-11-28 08:06:31 -0300

    incluido template para inserção de eventos na agenda gitignore para ignora os arquivos compilados

--- core/models.py
+++ core/models.py
@@ -9,7 +9,7 @@ class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
-    data_criacao = models.DateTimeField(auto_now=True, verbose_name='Data de criação')
+    data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
